545b2cf3330473d635eda8af7370fb877860dab8
Maruthi-S <72031156+Maruthi-S@users.noreply.github.com>
2020-12-18 18:15:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var gameState = 0, database;
+var playerCount , form;
+var game , player;
+var allPlayers,distance = 0;
+
+
+function setup(){
+  createCanvas(400,400);
+
+  database = firebase.database();
+  
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+ 
+  if(playerCount === 4 ){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
